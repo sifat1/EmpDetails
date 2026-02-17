875bf2c64a73a555b16bfa538144d136847b102a
sifat1 <sifatshikdhar@gmail.com>
2026-02-17 09:58:13 +0600

    fixed

--- EmpBackend/obj/Debug/net8.0/EmpBackend.AssemblyInfo.cs
+++ EmpBackend/obj/Debug/net8.0/EmpBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmpBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb95568f323f94b22659563c4bfa3d5ed60d81e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d97e87dd435ae3810fbfc19033d722b6f6683f44")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmpBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmpBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
